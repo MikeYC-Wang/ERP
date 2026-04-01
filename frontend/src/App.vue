0ff0fe776d4ddd361eada029414deeaeadff447a
MikeYC-Wang <asd384639@gmail.com>
2026-04-01 16:06:59 +0800

    chore: initial commit with basic environment

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="min-h-screen bg-stone-50 text-slate-900 dark:bg-slate-800 dark:text-stone-50 transition-colors duration-300">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class', // 支援切換深淺色模式
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
